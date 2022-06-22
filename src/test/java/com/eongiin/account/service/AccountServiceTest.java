107900d5a9a8890314d76b389010677b42d0b2e3
eongiin <eongiin@gmail.com>
2022-06-22 23:28:31 +0900

    test: controller, service(다양)

--- src/test/java/com/eongiin/account/service/AccountServiceTest.java
+++ src/test/java/com/eongiin/account/service/AccountServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,8 +17,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -26,7 +30,6 @@ class AccountServiceTest {
     private AccountRepository accountRepository;
     @InjectMocks
     private AccountService accountService;
-
 
     @Test
     @DisplayName("계좌 조회 성공")
@@ -37,12 +40,31 @@ class AccountServiceTest {
                         .accountStatus(AccountStatus.UNREGISTERED)
                         .accountNumber("65789")
                         .build()));
+
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+
         //when
         Account account = accountService.getAccount(455L);
 
         //then
-        assertEquals("65789",account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED,account.getAccountStatus());
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(455L, captor.getValue());
+        assertNotEquals(3L, captor.getValue());
+        assertTrue(455L == captor.getValue());
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void textFailedToSearchAccount() {
+        //given
+        //when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+        //then
+        assertEquals("Minus", exception.getMessage());
     }
 
     @Test
